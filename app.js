3e6f811082d09a2cdd84fd4935cb78431cae0b6c
Shun <ter0123540@gmail.com>
2022-06-27 16:16:36 +0800

    finish most of functions

--- app.js
+++ app.js
@@ -34,13 +34,33 @@ function update() {
 
 function render() {
   let allContent = "";
-  data.forEach((item) => {
-    allContent += `<li class="df aic position-relative" data-id=${item.id} ><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
-  });
-  list.innerHTML = allContent;
-  quantity.textContent = `${
-    data.filter((item) => item.status === "").length
-  } 個待完成項目`;
+  if (position === 1) {
+    data.forEach((item) => {
+      allContent += `<li class="df aic position-relative" data-id=${item.id} ><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
+    });
+    list.innerHTML = allContent;
+    quantity.textContent = `${
+      data.filter((item) => item.status === "").length
+    } 個待完成項目`;
+  } else if (position === 2) {
+    const yetItems = data.filter((item) => item.status === "");
+    yetItems.forEach((item) => {
+      allContent += `<li class="df aic position-relative" data-id=${item.id} ><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
+    });
+    list.innerHTML = allContent;
+    quantity.textContent = `${
+      data.filter((item) => item.status === "").length
+    } 個待完成項目`;
+  } else {
+    const doneItems = data.filter((item) => item.status === "checked");
+    doneItems.forEach((item) => {
+      allContent += `<li class="df aic position-relative" data-id=${item.id} ><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
+    });
+    list.innerHTML = allContent;
+    quantity.textContent = `${
+      data.filter((item) => item.status === "").length
+    } 個待完成項目`;
+  }
 }
 
 function addTodo(e) {
@@ -87,16 +107,66 @@ function toggleStatus(e) {
   const targetId = checkBox.parentNode.parentNode.getAttribute("data-id");
   const targetItem = data.filter((item) => item.id === parseInt(targetId))[0];
   const targetIndex = data.indexOf(targetItem);
+  checkBox.parentNode.classList.add("no-event");
 
   if (checkBox.checked === true) {
     axios.patch(url + targetId, { status: "checked" }).then((res) => {
       data[targetIndex].status = "checked";
+      render();
     });
   } else {
     axios.patch(url + targetId, { status: "" }).then((res) => {
       data[targetIndex].status = "";
+      render();
     });
   }
+}
+
+function deleteFinished(e) {
+  const finished = data.filter((item) => item.status === "checked");
+  if (finished < 1) return;
+
+  clearBtn.classList.add("no-event");
+
+  finished.forEach((finishedItem) => {
+    axios
+      .delete(url + finishedItem.id)
+      .then((res) => {
+        const targetItem = data.filter(
+          (item) => item.id === parseInt(finishedItem.id)
+        )[0];
+        const targetIndex = data.indexOf(targetItem);
+        data.splice(targetIndex, 1);
+        render();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
+
+function checkAll(e) {
+  position = 1;
+  all.classList.add("active");
+  yet.classList.remove("active");
+  done.classList.remove("active");
+  render();
+}
+
+function checkYet(e) {
+  position = 2;
+  yet.classList.add("active");
+  all.classList.remove("active");
+  done.classList.remove("active");
+  render();
+}
+
+function checkDone(e) {
+  position = 3;
+  done.classList.add("active");
+  all.classList.remove("active");
+  yet.classList.remove("active");
+  render();
 }
 
 addBtn.addEventListener("click", addTodo);
@@ -109,5 +179,13 @@ addContent.addEventListener("keyup", (e) => {
 list.addEventListener("click", deleteTodo);
 
 list.addEventListener("click", toggleStatus);
+
+clearBtn.addEventListener("click", deleteFinished);
+
+all.addEventListener("click", checkAll);
+
+yet.addEventListener("click", checkYet);
+
+done.addEventListener("click", checkDone);
 
 init();
--- test.js
+++ test.js
@@ -19,9 +19,6 @@ function deleteFinished(e) {
     const index = data.indexOf(item);
     data.splice(index, 1);
 
-    // delete unique number of finished from unique array
-    unique.splice(unique.indexOf(item.unique), 1);
-
     // delete data in the server
     axios.get(url).then((res) => {
       const allData = res.data;
@@ -44,41 +41,3 @@ function createUniqueNumber() {
     }
   }
 }
-
-clearBtn.addEventListener("click", deleteFinished);
-
-all.addEventListener("click", (e) => {
-  position = 1;
-  all.classList.add("active");
-  yet.classList.remove("active");
-  done.classList.remove("active");
-  render();
-});
-
-yet.addEventListener("click", (e) => {
-  position = 2;
-  yet.classList.add("active");
-  all.classList.remove("active");
-  done.classList.remove("active");
-
-  let allContent = "";
-  const yetFinished = data.filter((item) => item.status === "");
-  yetFinished.forEach((item, index) => {
-    allContent += `<li class="df aic position-relative" data-unique = ${item.unique} data-index=${index} "><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
-  });
-  list.innerHTML = allContent;
-});
-
-done.addEventListener("click", (e) => {
-  position = 3;
-  done.classList.add("active");
-  all.classList.remove("active");
-  yet.classList.remove("active");
-
-  let allContent = "";
-  const yetFinished = data.filter((item) => item.status === "checked");
-  yetFinished.forEach((item, index) => {
-    allContent += `<li class="df aic position-relative" data-unique = ${item.unique} data-index=${index} "><label class="label df aic"><input type="checkbox" class="hide" ${item.status}/><span class="check-mark position-relative border-radius-sm cursor-pointer"></span><span class="content pl-3">${item.content}</span></label><img src="/image/x.svg" class="delete-icon ml-a cursor-pointer" /></li>`;
-  });
-  list.innerHTML = allContent;
-});
